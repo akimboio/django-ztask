a0792d98098a63c4cbe572905c792a1be04c2a31
joshmarlow <josh.marlow@retickr.com>
2012-08-09 22:09:25 +0000

    * Added and updated some settings parameters

--- django_ztask/conf/settings.py
+++ django_ztask/conf/settings.py
@@ -1,10 +1,31 @@
 from django.conf import settings
 
-ZTASKD_URL = getattr(settings, 'ZTASKD_URL', 'tcp://127.0.0.1:5555')
+ZTASKD_WORKER_PORT = getattr(settings, 'ZTASKD_WORKER_PORT', 5555)
 ZTASKD_ALWAYS_EAGER = getattr(settings, 'ZTASKD_ALWAYS_EAGER', False)
 ZTASKD_DISABLED = getattr(settings, 'ZTASKD_DISABLED', False)
-ZTASKD_RETRY_COUNT = getattr(settings, 'ZTASKD_RETRY_COUNT', 5)
-ZTASKD_RETRY_AFTER = getattr(settings, 'ZTASKD_RETRY_AFTER', 5)
-
 ZTASKD_ON_LOAD = getattr(settings, 'ZTASKD_ON_LOAD', ())
-#ZTASKD_ON_CALL_COMPLETE = getattr(settings, 'ZTASKD_ON_COMPLETE', ())
+
+# URL for job workers; they listen and do
+ZTASKD_WORKER_URL_LIST = getattr(settings, 'ZTASKD_WORKER_URL_LIST', [])
+
+ZTASKD_WORKER_BIND_URL = getattr(settings, 'ZTASKD_WORKER_BIND_URL',
+    'tcp://0.0.0.0:{0}'.format(ZTASKD_WORKER_PORT))
+
+# URL used by job workers for dispatching
+# jobs to subworkers
+ZTASKD_SUBWORKER_MANAGEMENT_URL = getattr(
+    settings,
+    'ZTASKD_SUBWORKER_MANAGEMENT_URL',
+    'tcp://127.0.0.1:5556')
+
+# URL used by subworkers for sending job
+# status updates to the parent worker
+ZTASKD_SUBWORKER_STATUS_REPORT_URL = getattr(
+    settings,
+    'ZTASKD_SUBWORKER_STATUS_REPORT_URL',
+    'tcp://127.0.0.1:5557')
+
+ZTASKD_SUBWORKER_WORKER_START_PORT = getattr(
+    settings,
+    'ZTASKD_SUBWORKER_WORKER_START_PORT',
+    5560)
